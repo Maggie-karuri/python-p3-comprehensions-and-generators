84fd972cc7600968e4f8deac90c2fd11992dd8d7
Maggie-karuri <karurimargaret111@gmail.com>
2024-05-23 01:59:17 +0300

    list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if len(num_list) == 0:
+        return []
+    else:
+        even_numbers = [n for n in num_list if n % 2 == 0] 
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return []
+    else:
+        add_exclamations = []
+        for sentence in sentence_list:
+            add_exclamations.append(sentence + '!')
+    return add_exclamations
+
